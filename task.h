2f6f8dac17545906ffbd910b4fcfe8e07012f170
Gagan Nagaraj <gagandeep.konana@gmail.com>
2024-09-13 18:39:44 -0700

    requirements

---
+++ task.h
@@ -0,0 +1,22 @@
+
+
+/*
+Adds a new task to the system.
+Removes a task from the system by ID.
+Finds and returns a pointer to the task with the given ID. Returns nullptr if not found.
+Returns a list of tasks with the given status.
+Returns a list of all tasks in the system.
+Updates the status of a task by ID.
+
+
+noun and verbs:
+ID (integer, unique identifier)
+Title (string)
+Status (enum: ToDo, InProgress, Done)
+Priority (enum: Low, Medium, High)
+
+task: 
+system: list <tasks*>
+
+
+*/
